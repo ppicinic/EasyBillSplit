3e9ba438c6ec183528ee62fb4b4a1ab92bd96826
Phil Picinic <philpicinic@gmail.com>
2014-10-02 01:50:39 -0400

    can click on contact and retrieve numbers

--- EasyBillSplit/app/src/main/java/com/philpicinic/easybillsplit/dialogs/ContactNumberSearchFragment.java
+++ EasyBillSplit/app/src/main/java/com/philpicinic/easybillsplit/dialogs/ContactNumberSearchFragment.java
@@ -7,11 +7,13 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Contacts;
 import android.provider.ContactsContract;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -21,32 +23,30 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 import com.philpicinic.easybillsplit.R;
+import com.philpicinic.easybillsplit.adapters.NumberAdapter;
 
 /**
  * Created by Phil on 10/1/2014.
  */
-public class ContactSearchFragment extends DialogFragment implements
+public class ContactNumberSearchFragment extends DialogFragment implements
         LoaderManager.LoaderCallbacks<Cursor>, AdapterView.OnItemClickListener{
 
     @SuppressLint("InlinedApi")
     private final static String[] FROM_COLUMNS = {
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ?
-                    ContactsContract.Contacts.DISPLAY_NAME_PRIMARY :
-                    ContactsContract.Contacts.DISPLAY_NAME
+            ContactsContract.CommonDataKinds.Phone.NUMBER,
+            ContactsContract.CommonDataKinds.Phone.TYPE
     };
 
     public static final int[] TO_IDS = {
-            android.R.id.text1
+            R.id.contact_number,
+            R.id.contact_type
     };
 
     @SuppressLint("InlinedApi")
     private static final String[] PROJECTION =
             {
-                    ContactsContract.Contacts._ID,
-                    ContactsContract.Contacts.LOOKUP_KEY,
-                    Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ?
-                            ContactsContract.Contacts.DISPLAY_NAME_PRIMARY :
-                            ContactsContract.Contacts.DISPLAY_NAME
+                    ContactsContract.CommonDataKinds.Phone.NUMBER,
+                    ContactsContract.CommonDataKinds.Phone.TYPE
             };
 
     private static final int CONTACT_ID_INDEX = 0;
@@ -54,45 +54,45 @@ public class ContactSearchFragment extends DialogFragment implements
 
     @SuppressLint("InlinedApi")
     private static final String SELECTION =
-                    ContactsContract.Contacts.HAS_PHONE_NUMBER + ">0";
+            ContactsContract.Contacts.HAS_PHONE_NUMBER + ">0";
 
 
     private long mContactId;
     private String mContactKey;
     private Uri mContactUri;
-    private SimpleCursorAdapter mCursorAdapter;
+    private CursorAdapter mCursorAdapter;
     private ListView mContactList;
     private View v;
     private EditText searchBar;
     private String mSearchString;
     private String[] mSelectionArgs = { ">0" };
-    private ContactSearchFragment instance;
+    private long id;
+    private String key;
 
-    public ContactSearchFragment() {}
+    public ContactNumberSearchFragment() {}
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState){
         mSearchString = "";
-        instance = this;
         v = View.inflate(getActivity(), R.layout.contact_list, null);
-        searchBar = (EditText) v.findViewById(R.id.search_bar);
-        searchBar.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
-                mSearchString = searchBar.getText().toString();
-                initLoad();
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
+//        searchBar = (EditText) v.findViewById(R.id.search_bar);
+//        searchBar.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+//                mSearchString = searchBar.getText().toString();
+//                initLoad();
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//
+//            }
+//        });
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setView(v);
         return builder.create();
@@ -105,69 +105,45 @@ public class ContactSearchFragment extends DialogFragment implements
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
+        id = getArguments().getLong("ID");
+        key = getArguments().getString("KEY");
         mContactList = (ListView) v.findViewById(R.id.contact_list_view);
-        mCursorAdapter = new SimpleCursorAdapter(
+        mCursorAdapter = new NumberAdapter(
                 getActivity(),
-                R.layout.contact_item,
-                null,
+                R.layout.contact_number_item,
                 FROM_COLUMNS,
                 TO_IDS,
                 0);
         mContactList.setAdapter(mCursorAdapter);
 
-//        mContactList.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                System.out.println("test");
-//            }
-//        });
-//        mContactList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                System.out.println("happens");
-//            }
-//        });
         mContactList.setOnItemClickListener(this);
         getLoaderManager().initLoader(0, null, this);
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View item, int position, long rowID){
-        System.out.println("happens");
         Cursor cursor = mCursorAdapter.getCursor();
         cursor.moveToPosition(position);
-        mContactId = cursor.getLong(CONTACT_ID_INDEX);
-        mContactKey = cursor.getString(LOOKUP_KEY_INDEX);
-        mContactUri = ContactsContract.Contacts.getLookupUri(mContactId, mContactKey);
-        Bundle args = new Bundle();
-        args.putLong("ID", mContactId);
-        args.putString("KEY", mContactKey);
-        ContactNumberSearchFragment newDialog = new ContactNumberSearchFragment();
-        newDialog.setArguments(args);
-        getDialog().cancel();
-        newDialog.show(getFragmentManager().beginTransaction(), null);
-        System.out.println("happens");
-
+//        mContactId = cursor.getLong(CONTACT_ID_INDEX);
+//        mContactKey = cursor.getString(LOOKUP_KEY_INDEX);
+//        mContactUri = ContactsContract.Contacts.getLookupUri(mContactId, mContactKey);
     }
 
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        Uri contentUri;
-        if(mSearchString.length() == 0){
-            contentUri = ContactsContract.Contacts.CONTENT_URI;
-        }else {
-            contentUri = Uri.withAppendedPath(
-                    ContactsContract.Contacts.CONTENT_FILTER_URI,
-                    Uri.encode(mSearchString)
-            );
-        }
+        Uri contentUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+//        contentUri = Uri.withAppendedPath(ContactsContract.CommonDataKinds.Phone.CONTENT_FILTER_URI, Uri.encode(""+id));
+        System.out.println(ContactsContract.CommonDataKinds.Phone.CONTACT_ID);
+        System.out.println(ContactsContract.CommonDataKinds.Phone.NUMBER);
+
         return new CursorLoader(
                 getActivity(),
                 contentUri,
-                PROJECTION,
-                SELECTION,
                 null,
-                PROJECTION[2] + " ASC"
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + id,
+//                null,
+                null,
+                null
         );
     }
 

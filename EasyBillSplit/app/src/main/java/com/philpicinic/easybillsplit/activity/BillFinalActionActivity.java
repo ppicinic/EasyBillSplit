699f13c4ef932fed9bfe801e5aefd376ab042d71
Phil Picinic <philpicinic@gmail.com>
2014-10-09 01:39:46 -0400

    tip and tax now in fragments

--- EasyBillSplit/app/src/main/java/com/philpicinic/easybillsplit/activity/BillFinalActionActivity.java
+++ EasyBillSplit/app/src/main/java/com/philpicinic/easybillsplit/activity/BillFinalActionActivity.java
@@ -1,16 +1,24 @@
 package com.philpicinic.easybillsplit.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckedTextView;
 import android.widget.EditText;
 
 import com.philpicinic.easybillsplit.R;
+import com.philpicinic.easybillsplit.fragments.TaxFragment;
+import com.philpicinic.easybillsplit.fragments.TipFragment;
 import com.philpicinic.easybillsplit.item.IItem;
 import com.philpicinic.easybillsplit.service.ManagerService;
 
@@ -29,6 +37,53 @@ public class BillFinalActionActivity extends BaseActionBarActivity {
 
         final EditText taxText = (EditText) findViewById(R.id.tax_input);
         final EditText tipText = (EditText) findViewById(R.id.tip_input);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        boolean taxCheck = prefs.getBoolean("PREF_TAX_CHECK", true);
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        final TaxFragment taxFragment = (TaxFragment) fm.findFragmentById(R.id.tax_fragment);
+        if(!taxCheck && !taxFragment.isHidden()){
+            ft.hide(taxFragment);
+        }else if(taxCheck && taxFragment.isHidden()){
+            ft.show(taxFragment);
+        }
+        final CheckedTextView includeTax = (CheckedTextView) findViewById(R.id.include_tax);
+        includeTax.setChecked(taxCheck);
+        includeTax.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                includeTax.setChecked(!includeTax.isChecked());
+                if(includeTax.isChecked() && taxFragment.isHidden()){
+                    getSupportFragmentManager().beginTransaction().show(taxFragment).commit();
+                }else if(!includeTax.isChecked() && !taxFragment.isHidden()){
+                    getSupportFragmentManager().beginTransaction().hide(taxFragment).commit();
+                }
+            }
+        });
+
+        boolean tipCheck = prefs.getBoolean("PREF_TIP_CHECK", true);
+        final TipFragment tipFragment = (TipFragment) fm.findFragmentById(R.id.tip_fragment);
+        if(!tipCheck && !tipFragment.isHidden()){
+            ft.hide(tipFragment);
+        }else if(tipCheck && tipFragment.isHidden()){
+            ft.show(tipFragment);
+        }
+        ft.commit();
+        final CheckedTextView includeTip = (CheckedTextView) findViewById(R.id.include_tip);
+        includeTip.setChecked(tipCheck);
+        includeTip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                includeTip.setChecked(!includeTip.isChecked());
+                if(includeTip.isChecked() && tipFragment.isHidden()){
+                    getSupportFragmentManager().beginTransaction().show(tipFragment).commit();
+                }else if(!includeTip.isChecked() && !tipFragment.isHidden()){
+                    getSupportFragmentManager().beginTransaction().hide(tipFragment).commit();
+                }
+            }
+        });
+
 
         BigDecimal total = new BigDecimal(0);
         for(IItem item : items){
